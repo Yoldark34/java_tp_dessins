887066e69672370b02420214c062e7d541fdee20
Yoldark34 <yoldark@gmail.com>
2013-03-14 22:59:51 +0100

    Check if it is the left click of the mouse for mouse pressed and released

--- src/Vue/Listener/SimpleMouseListener.java
+++ src/Vue/Listener/SimpleMouseListener.java
@@ -8,6 +8,7 @@ import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.util.Iterator;
 import java.util.List;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -44,14 +45,16 @@ public class SimpleMouseListener extends JCanvasMouseListener {
      */
     @Override
     protected void mouseReleasedAction(MouseEvent e) {
-        if (this.resizeMode) {
-            canvas.resizeDrawable(this.polygoneToResize, this.pointStart, e.getPoint());
-            this.resizeMode = false;
-            canvas.reColor(this.polygoneToResize, Color.BLACK);
-            this.canvas.desactiveElement();
-        } else {
-            this.pointStart = null;
-            this.resizeMode = false;
+        if (SwingUtilities.isLeftMouseButton(e)) {
+            if (this.resizeMode) {
+                canvas.resizeDrawable(this.polygoneToResize, this.pointStart, e.getPoint());
+                this.resizeMode = false;
+                canvas.reColor(this.polygoneToResize, Color.BLACK);
+                this.canvas.desactiveElement();
+            } else {
+                this.pointStart = null;
+                this.resizeMode = false;
+            }
         }
     }
 
@@ -63,12 +66,14 @@ public class SimpleMouseListener extends JCanvasMouseListener {
      */
     @Override
     protected void mousePressedAction(MouseEvent e) {
-        List selectedDrawables = super.canvas.findDrawables(e.getPoint());
-        if (!selectedDrawables.isEmpty()) {
-            this.polygoneToResize = (Polygone) selectedDrawables.get(0);
-            this.pointStart = e.getPoint();
-            this.resizeMode = true;
-            canvas.active((Polygone) selectedDrawables.get(0));
+        if (SwingUtilities.isLeftMouseButton(e)) {
+            List selectedDrawables = super.canvas.findDrawables(e.getPoint());
+            if (!selectedDrawables.isEmpty()) {
+                this.polygoneToResize = (Polygone) selectedDrawables.get(0);
+                this.pointStart = e.getPoint();
+                this.resizeMode = true;
+                canvas.active((Polygone) selectedDrawables.get(0));
+            }
         }
     }
 
